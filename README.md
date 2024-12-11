da893c4c0a0a4ca67cdbe100223fe3a9014da0ba
LezardSC <117669219+LezardSC@users.noreply.github.com>
2024-12-11 22:52:09 +0100

    set-up project

---
+++ README.md
@@ -0,0 +1,7 @@
+`npm init`
+`npm install hardhat`
+`npx hardhat` (Javascript project, yes to everything)
+`npm i ethers`
+`npm install @nomiclabs/hardhat-etherscan`
+`npm install @openzeppelin/contracts`
+`npm install dotenv`
---
+++ contracts/Girl42.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract Girl42 is ERC721 {
+	constructor(string memory name, string memory symbol)
+		ERC721(name, symbol) {
+
+	}
+}
